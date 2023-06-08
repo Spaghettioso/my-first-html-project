eb856286b035cba333bd572169064d1d9cedf463
Jonathan OReilly <jonathanoreilly@live.co.uk>
2023-06-08 13:33:19 +0100

    adding a js document making a prompt asking for your name and gives a respon but if you put opopen the pod bay doors hal it gives another message

---
+++ app.js
@@ -0,0 +1,9 @@
+
+let spaceOdysseyReference = prompt("Hello, my name is HAL 9000. what is your name?");
+
+if (spaceOdysseyReference === "open the pod bay doors hal") {
+    alert ("I'm afraid I cant do that Dave");
+} else {
+    alert ("nice to meet you " + spaceOdysseyReference);
+}
+
